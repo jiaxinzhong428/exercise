85688a970c451a0b67ebc29b7e1d944511e88484
钟嘉欣 <jiaxinzhong428@163.com>
2019-04-18 11:47:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" alt="">
+    <h1>{{msg}}</h1>
+    <firstcomponent></firstcomponent>
+    <secondcomponent></secondcomponent>
+  </div>
+</template>
+<script>
+import firstcomponent from "./component/firstcomponent.vue";
+import secondcomponent from "./component/secondcomponent.vue";
+export default {
+  data() {
+    return {
+      msg: "hello vue!"
+    };
+  },
+  components: {
+    firstcomponent,
+    secondcomponent
+  }
+};
+</script>
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
+
+
